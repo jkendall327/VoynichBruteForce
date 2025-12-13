ae254a5f8a9f8fcc1f119cc6d3a62a703ba2b32a
jackkendall <jkendall3096@gmail.com>
2025-12-13 15:49:18 +0000

    Minor refactor

--- VoynichBruteForce/Infrastructure/ServiceCollectionExtensions.cs
+++ VoynichBruteForce/Infrastructure/ServiceCollectionExtensions.cs
@@ -8,37 +8,40 @@ namespace VoynichBruteForce;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddVoynichServices(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddSingleton<IGenomeFactory, DefaultGenomeFactory>();
-        services.AddSingleton<ISourceTextRegistry, DefaultSourceTextRegistry>();
-        services.AddSingleton<IRankerProvider, DefaultRankerProvider>();
-        services.AddSingleton<RandomFactory>();
-        services.AddSingleton<PipelineRunner>();
-        services.AddSingleton<EvolutionEngine>();
-    }
-
-    public static void AddVoynichConfiguration(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.Configure<ServiceProviderOptions>(options =>
+        public void AddVoynichServices()
         {
-            options.ValidateOnBuild = true;
-            options.ValidateScopes = true;
-        });
+            services.AddSingleton<IGenomeFactory, DefaultGenomeFactory>();
+            services.AddSingleton<ISourceTextRegistry, DefaultSourceTextRegistry>();
+            services.AddSingleton<IRankerProvider, DefaultRankerProvider>();
+            services.AddSingleton<RandomFactory>();
+            services.AddSingleton<PipelineRunner>();
+            services.AddSingleton<EvolutionEngine>();
+        }
 
-        services
-            .AddOptions<AppSettings>()
-            .Bind(configuration.GetSection(AppSettings.SectionName))
-            .ValidateOnStart();
+        public void AddVoynichConfiguration(IConfiguration configuration)
+        {
+            services.Configure<ServiceProviderOptions>(options =>
+            {
+                options.ValidateOnBuild = true;
+                options.ValidateScopes = true;
+            });
 
-        services
-            .AddOptions<VoynichProfile>()
-            .Bind(configuration.GetSection(VoynichProfile.SectionName))
-            .ValidateOnStart();
+            services
+                .AddOptions<AppSettings>()
+                .Bind(configuration.GetSection(AppSettings.SectionName))
+                .ValidateOnStart();
 
-        services
-            .AddOptions<Hyperparameters>()
-            .Bind(configuration.GetSection(Hyperparameters.SectionName))
-            .ValidateOnStart();
+            services
+                .AddOptions<VoynichProfile>()
+                .Bind(configuration.GetSection(VoynichProfile.SectionName))
+                .ValidateOnStart();
+
+            services
+                .AddOptions<Hyperparameters>()
+                .Bind(configuration.GetSection(Hyperparameters.SectionName))
+                .ValidateOnStart();
+        }
     }
 }
