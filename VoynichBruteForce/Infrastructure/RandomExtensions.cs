90bd507e6cb80f4876672a4517b25bf23093f9ec
jackkendall <jkendall3096@gmail.com>
2025-12-14 10:40:46 +0000

    Extension block

--- VoynichBruteForce/Infrastructure/RandomExtensions.cs
+++ VoynichBruteForce/Infrastructure/RandomExtensions.cs
@@ -2,53 +2,55 @@ namespace VoynichBruteForce;
 
 public static class RandomExtensions
 {
-    public static T RandomEnumMember<T>(this Random random) where T : struct, Enum
+    extension(Random random)
     {
-        var values = Enum.GetValues<T>();
+        public T RandomEnumMember<T>() where T : struct, Enum
+        {
+            var values = Enum.GetValues<T>();
 
-        random.Shuffle(values);
+            random.Shuffle(values);
 
-        return values.First();
-    }
+            return values.First();
+        }
 
-    /// <summary>
-    /// Returns a random element from an array.
-    /// </summary>
-    public static T NextItem<T>(this Random random, T[] array)
-    {
-        return array[random.Next(array.Length)];
-    }
+        /// <summary>
+        /// Returns a random element from an array.
+        /// </summary>
+        public T NextItem<T>(T[] array)
+        {
+            return array[random.Next(array.Length)];
+        }
 
-    /// <summary>
-    /// Returns a random element from a collection.
-    /// </summary>
-    public static T NextItem<T>(this Random random, IReadOnlyList<T> list)
-    {
-        return list[random.Next(list.Count)];
-    }
-    
-    public static T NextItem<T>(this Random random, ReadOnlySpan<T> list)
-    {
-        return list[random.Next(list.Length)];
-    }
+        /// <summary>
+        /// Returns a random element from a collection.
+        /// </summary>
+        public T NextItem<T>(IReadOnlyList<T> list)
+        {
+            return list[random.Next(list.Count)];
+        }
 
-    /// <summary>
-    /// Returns a random boolean value.
-    /// </summary>
-    public static bool NextBool(this Random random)
-    {
-        return random.Next(2) == 0;
-    }
+        public T NextItem<T>(ReadOnlySpan<T> list)
+        {
+            return list[random.Next(list.Length)];
+        }
 
-    /// <summary>
-    /// Returns a random character in the specified range.
-    /// </summary>
-    /// <param name="random">The random generator</param>
-    /// <param name="start">The starting character (e.g., 'a')</param>
-    /// <param name="count">The number of characters in the range (e.g., 26 for a-z)</param>
-    /// <returns>A random character in the range [start, start+count)</returns>
-    public static char NextChar(this Random random, char start, int count)
-    {
-        return (char)(start + random.Next(count));
+        /// <summary>
+        /// Returns a random boolean value.
+        /// </summary>
+        public bool NextBool()
+        {
+            return random.Next(2) == 0;
+        }
+
+        /// <summary>
+        /// Returns a random character in the specified range.
+        /// </summary>
+        /// <param name="start">The starting character (e.g., 'a')</param>
+        /// <param name="count">The number of characters in the range (e.g., 26 for a-z)</param>
+        /// <returns>A random character in the range [start, start+count)</returns>
+        public char NextChar(char start, int count)
+        {
+            return (char)(start + random.Next(count));
+        }
     }
 }
